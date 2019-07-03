98718aa17dc35ead380b126176e078c6d7317264
Ariel Stitt <arielstitt@Skills-Macbook-Pro-37.local>
2019-07-03 15:11:13 -0400

    first commit

---
+++ app.js
@@ -0,0 +1,16 @@
+// // var buttonOne = document.getElementById('button-1')
+
+// // buttonOne.addEventListener('click', function(){
+// //     // console log the button was clicked
+// //     console.log("button one clicked!!!")
+// //     alert('button one clicked!')
+// // })
+
+// var buttonTwo = document.getElementById('button-2')
+
+// buttonTwo.addEventListener('click', function () {
+//     alert('button 2 clicked!')
+// })
+// function buttonOne() {
+//     console.log("button was clicked")
+// }
